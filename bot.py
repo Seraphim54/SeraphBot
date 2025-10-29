d010a50e38fbd3b1f7603a65f5abbbc75d29397e
Joseph Hensley <joseph.hensley@outlook.com>
2025-10-28 21:42:16 -0500

    various function testing
    Changes to be committed:
    	modified:   bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ async def hello(ctx):
 
 @bot.command()
 async def dave(ctx):
-    await ctx.send("Now, Dave. I’m afraid I can’t do that.")
+    await ctx.send("Now, Dave. I'm afraid I can't do that.")
 
 @bot.command()
 async def deathsave(ctx):
@@ -38,5 +38,61 @@ async def deathsave(ctx):
         result = "☠️ Fail! You are one step closer to Death."
 
     await ctx.send(f"You rolled a {roll}: {result}")
+
+# -- Choice Tuples
+
+alignments = ("Lawful Good", "Neutral Good", "Chatoic Good", "Lawful Neutral", "True Neutral", "Chaotic Neutral", "Lawful Evil", "Neutral Evil", "Chaotic Evil")
+
+races = ("Aarakocra", "Aasimar", "Astral Elf", "Autognome", "Bugbear", "Centaur", "Changeling", "Custom Lineage", "Deep Gnome", "Dhampir", "Dragonborn", "Duergar", "Dwarf", "Eladrin", "Elf", "Fairy", "Firbolg", "Genasi", "Giff", "Githyanki", "Githzerai", "Gnome", "Goblin", "Goliath", "Grung", "Hadozee", "Half-Elf", "Half-Orc", "Halfling", "Harengon", "Hexblood", "Hobgoblin", "Human", "Kalashtar", "Kender", "Kenku", "Kobold", "Leonin", "Lizardfolk", "Locathah", "Loxodon", "Minotaur", "Orc", "Owlin", "Plasmoid", "Reborn", "Satyr", "Sea Elf", "Shadar-Kai", "Shifter", "Simic Hybrid", "Tabaxi", "Thri-kreen", "Tiefling", "Tortle", "Triton", "Vedalken", "Verdan", "Warforged", "Yuan-ti")
+
+classes = ("Artificer", "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard")
+
+@bot.command()
+async def random_build(ctx):
+    rand_align = random.choice(alignments)
+    rand_race = random.choice(races)
+    rand_class = random.choice(classes)
+
+    await ctx.send(f"🐉 Go forth and seek adventure with your shiny new {rand_align} {rand_race} {rand_class}")
+
+@bot.command()
+async def mmn(ctx):
+    await ctx.send("🤓 Eugene 🤓")
+
+@bot.command()
+async def newstats(ctx):
+    def roll_stat():
+        rolls = [random.randint(1, 6) for v in range(4)]
+        top_three = sorted(rolls, reverse=True)[:3]
+        return sum(top_three)
+
+    def roll_stat_block(min_total=72):
+        attempts = 0
+
+        while True:
+            attempts += 1
+            stats = [roll_stat() for s in range(6)]
+            if sum(stats) >= min_total:
+                return stats, attempts
+
+    stats, tries = roll_stat_block()
+    await ctx.send(f"Rolled in {tries} attempt(s): `{stats}` (Total: {sum(stats)})")
+
+@bot.command()
+async def bnuuy(ctx):
+    bunny_gifs = [
+        "https://tenor.com/JBF3fq5wKL.gif",
+        "https://tenor.com/view/bunny-cute-bun-rabbit-dance-gif-12204421675478327501",
+        "https://tenor.com/view/bunny-eating-gif-27018618",
+        "https://tenor.com/view/albert-harebrayne-bunny-rabbit-gif-10500821922776666435",
+        "https://tenor.com/view/bunny-kissing-dog-bunny-dog-kissing-puppy-gif-10364923600939704626",
+    ]
+    gif = random.choice(bunny_gifs)
+    await ctx.send(gif)
+
+@bot.command()
+async def mothman(ctx):
+    mothtwerk = "https://art.ngfiles.com/images/3435000/3435336_codingcanine_thicc-mothman.gif"
+    await ctx.send(mothtwerk)
 
 bot.run(TOKEN)
